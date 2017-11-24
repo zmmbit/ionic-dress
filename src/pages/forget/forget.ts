45429b332ce50b172275866dcecebd9d120be3df
wenyingxin <wenyingxin2010@sina.com>
2017-11-24 10:21:44 +0800

    forget

--- src/pages/forget/forget.ts
+++ src/pages/forget/forget.ts
@@ -15,11 +15,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ForgetPage {
 
+  params={
+    usertel:"",
+    newpass:'',
+    vcode:'',
+    sure_pwd:''
+  }
+
+  codeParam = {
+    fromflag:2,
+    usertel:""
+  }
+
+  // 验证码倒计时
+  verifyCode: any = {
+    verifyCodeTips: "获取验证码",
+    countdown: 60,
+    disable: false
+  }
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ForgetPage');
+  }
+
+  settime(){
+    if(this.verifyCode.countdown == 1){
+      this.verifyCode.countdown = 60;
+      this.verifyCode.verifyCodeTips="获取验证码";
+      this.verifyCode.disable = true;
+      return;
+    }else{
+      this.verifyCode.countdown--;
+    }
+    this.verifyCode.verifyCodeTips="重新获取("+this.verifyCode.countdown+")";
+    setTimeout(()=>{
+      this.verifyCode.verifyCodeTips="重新获取("+this.verifyCode.countdown+")";
+      this.settime();
+    },1000);
+  }
+
+  getCode(){
+    if(this.codeParam.usertel == ''){
+      console.log("请填写手机号...");
+      return;
+    }
+    //倒计时开始
+    this.verifyCode.disable = true;
+    this.settime();
   }
 
 }
